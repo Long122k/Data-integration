913480d1c95a125c12eedc41ab92a5abbd1fdb43
locnv <nguyenvanloc432k@gmail.com>
2022-06-13 00:05:01 +0700

    create fptshop spider

---
+++ crawler/crawler/spiders/fptshop.py
@@ -0,0 +1,31 @@
+
+import scrapy
+
+
+class TGDDSpider(scrapy.Spider):
+  name = 'TGDD'
+  allowed_domains = ['https://fptshop.com.vn']
+  start_urls = ['https://fptshop.com.vn/dien-thoai']
+  root_url = 'https://fptshop.com.vn'
+
+  def parse(self, response):
+    list_branchs_url = response.xpath('//div[contains(@class,"swiper-slide")]/a/@href').getall()
+
+    list_branchs_url = list(map(lambda branch_url: self.root_url + branch_url, list_branchs_url))
+
+    # for branch_url in list_branchs_url:
+    #   yield scrapy.Request(branch_url, self.parse_branch)
+
+    yield scrapy.Request(list_branchs_url[0], self.parse_branch)
+
+
+  def parse_branch(self, response):
+    list_phone_urls = response.xpath('//div[@class="cdt-product__img"]/a/@href').getall()
+
+    list_phone_urls = list(map(lambda branch_url: self.root_url + branch_url, list_phone_urls))
+
+    yield scrapy.Request(list_phone_urls[0], self)
+    pass
+
+
+  def parse_
